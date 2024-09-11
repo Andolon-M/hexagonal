01e074a80c317a64ba5f2cbaced45abce9cbd139
Andolon-M <aemendez-l@outlook.com>
2024-09-11 15:35:44 -0500

    feat: :construction: configuracion basica de backend y frondend terminada

--- api/infrastructure/database/mongodb.cjs
+++ api/infrastructure/database/mongodb.cjs
@@ -6,7 +6,7 @@ class ConnectToDatabase{
     connection;
     user;
     #password;
-    constructor({user, pwd} = {user: process.env.MONGO_USER, pwd: process.env.MONGO_PWD}){
+    constructor({user, pwd} = {user: process.env.VITE_MONGO_USER, pwd: process.env.VITE_MONGO_PWD}){
         if(ConnectToDatabase.instanceConnect && this.connection){
             return ConnectToDatabase.instanceConnect;
         }
@@ -16,10 +16,10 @@ class ConnectToDatabase{
         ConnectToDatabase.instanceConnect = this;
     }
     async connectOpen(){
-        this.connection = new MongoClient(`${process.env.MONGO_ACCESS}${this.user}:${this.getPassword}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.connection = new MongoClient(`${process.env.VITE_MONGO_ACCESS}${this.user}:${this.getPassword}@${process.env.VITE_MONGO_HOST}:${process.env.VITE_MONGO_PORT}`, { useNewUrlParser: true, useUnifiedTopology: true });
         try {
             await this.connection.connect();
-            this.db = this.connection.db(process.env.MONGO_DB_NAME);
+            this.db = this.connection.db(process.env.VITE_MONGO_DB_NAME);
         } catch (error) {
             this.connection = undefined;
             throw new Error('Error connecting');
