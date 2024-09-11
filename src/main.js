19e599c4ddd5a73cf9fed1edbae90efe5298c645
Andolon-M <aemendez-l@outlook.com>
2024-09-11 16:46:11 -0500

    feat: :construction: configuracion de re-direccionamiento de la protecion de login

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './Home.vue';
 import Login from './Login.vue'
-import Product from './product.vue';
+import Product from './Product.vue';
 import Createuser from './Createuser.vue';
+import Cookies from 'js-cookie'; // para manejar cookies
 const routes = [
     { path: '/', component: Home },
     { path: '/login', name: 'Login', component: Login },
     { path: '/register', name: 'Register', component: Createuser },
-    { path: '/products', name: 'Products', component: Product},
+    { path: '/products', name: 'Products', component: Product, meta: { requiresAuth: true }},
 
 ]
 
@@ -19,7 +20,23 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 });
-
+// Guardia global para verificar cookies
+router.beforeEach((to, from, next) => {
+    // Si la ruta requiere autenticación
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        const token = Cookies.get('token'); // Verifica la cookie 'token' (o la cookie que uses para la autenticación)
+        
+        if (!token) {
+            // Si no hay cookie, redirige al login
+            next({ name: 'Login' });
+        } else {
+            // Si hay cookie, permite el acceso
+            next();
+        }
+    } else {
+        next(); // Si no requiere autenticación, sigue normalmente
+    }
+});
 //instancia de vue
 const app = createApp(App)
 
