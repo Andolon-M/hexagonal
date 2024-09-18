a823586e65dae28ad51189dd58eb984c1fcc9eb2
Andolon-M <aemendez-l@outlook.com>
2024-09-18 17:44:31 -0500

    feat: :sparkles: correccion para evitar que se envie el el usuario en mayusculas durante el login

--- src/Login.vue
+++ src/Login.vue
@@ -66,7 +66,7 @@ const handleSubmit = async () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                nick: nick.value,
+                nick: nick.value.toUpperCase(),
                 password: password.value
             })
         });
